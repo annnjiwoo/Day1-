f028d381b4fd6265071176463e8ba2e386e16e61
annnjiwoo <123406330+annnjiwoo@users.noreply.github.com>
2023-02-05 21:42:38 +0900

    Add files via upload

---
+++ day01/homework1.java
@@ -0,0 +1,42 @@
+package day01;
+
+public class homework1 {
+    public static void main(String[] args) {
+        System.out.println("과제 1 강아지 아닌교!");
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"\'    |");
+        System.out.println("||_/=\\\\__|");
+        System.out.println("\n");
+        System.out.println("과제 2 낙엽~");
+        System.out.println("         ,r\'\"7");
+        System.out.println("r\'-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   \'~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("\n");
+        System.out.println("과제 3 돌고래");
+        System.out.println("            __   __");
+        System.out.println("           __ \\ / __");
+        System.out.println("          /  \\ | /  \\");
+        System.out.println("              \\|/");
+        System.out.println("         _,.---v---._");
+        System.out.println("/\\__/\\  /            \\");
+        System.out.println("\\_  _/ /              \\");
+        System.out.println("  \\ \\_|           @ __|");
+        System.out.println("   \\                \\_");
+        System.out.println("    \\     ,__/       /");
+        System.out.println("  ~~~\'~~~~~~~~~~~~~~/~~~~");
+        System.out.println("\n");
+        System.out.println("과제 4 마지막 코끼리 ㅋ");
+        System.out.println("          __     __");
+        System.out.println("         /  \\~~~/  \\");
+        System.out.println("   ,----(     ..    )");
+        System.out.println("  /      \\__     __/");
+        System.out.println(" /|         (\\  |(");
+        System.out.println("^ \\   /___\\  /\\ |");
+        System.out.println("   |__|   |__|-\"");
+    }
+}
